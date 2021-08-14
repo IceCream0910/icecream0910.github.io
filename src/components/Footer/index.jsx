a8afd778f71808af25b1f06cb73a3d443771d957
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2021-08-14 21:56:03 +0900

    footer fix

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,9 +6,7 @@ const Footer = () => {
   return (
     <div className='footer-container'>
       <a
-        target='_blank'
         rel='noopener noreferrer'
-        href='https://github.com/ddongule/ddongule-simple-portfolio'
       >
         <p className='footer-contents'>
           <span className='icon'>&copy; 2021</span> Ice Cream
